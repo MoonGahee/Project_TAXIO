4bc32a51494ccd3cb4acb4fde478e2e7517fadbd
KooHaeun <heni1325@gmail.com>
2020-07-02 17:46:32 +0900

    general_xml

---
+++ app/src/main/java/com/example/project_taxio/general_s_regionActivity.java
@@ -0,0 +1,96 @@
+package com.example.project_taxio;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.taxio.R;
+import com.example.taxio.select_scheduleActivity;
+
+public class general_s_regionActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TextView title_text;
+    ImageView map;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_region);
+
+        map = findViewById(R.id.imageKorea);
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(NewScheduleRegion.this);
+                builder.setTitle("지역 확인");
+                builder.setMessage("선택하신 지역이 제주도가 맞습니까?");
+                builder.setPositiveButton("네", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(NewScheduleRegion.this, select_scheduleActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.show();
+            }
+        });
+
+        toolbar = (Toolbar)findViewById(R.id.appbar); // 툴바를 액티비티의 앱바로 지정
+        setSupportActionBar(toolbar); //툴바를 현재 액션바로 설정
+        ActionBar actionBar = getSupportActionBar(); //현재 액션바를 가져옴
+        actionBar.setDisplayShowTitleEnabled(false); //액션바의 타이틀 삭제
+        actionBar.setDisplayHomeAsUpEnabled(true); //홈으로 가기 버튼 활성화
+
+        title_text = findViewById(R.id.title_text);
+        title_text.setClickable(true);
+
+        title_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+    public void goMain(ImageView logo){//로고버튼 클릭시
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {//toolbar의 back키 눌렀을 시
+        switch (item.getItemId()){
+            case android.R.id.home:{//이전 화면으로 돌아감
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
