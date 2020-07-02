ad4fd996693fc74fa34054ba32f5aada1d39e505
Kwanwoo-park <65990897+Kwanwoo-park@users.noreply.github.com>
2020-07-02 17:49:42 +0900

    실험

--- app/src/main/java/com/example/project_taxio/MainActivity.java
+++ app/src/main/java/com/example/project_taxio/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
                 } else if (id == R.id.drawer_myInfo) {
 
+
                 } else if (id == R.id.drawer_setting) {
 
                 }
